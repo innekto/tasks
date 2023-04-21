94c66a442938307dbf8c785f1f97874822a01a03
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-04-21 14:05:14 +0300

    done

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,17 +1,13 @@
 import { useDispatch } from 'react-redux';
-import { deleteTask, toggleCompleted } from 'redux/actions';
-
 import { MdClose } from 'react-icons/md';
+import { deleteTask, toggleCompleted } from 'redux/tasksSlice';
 import css from './Task.module.css';
 
 export const Task = ({ task }) => {
-  // Отримуємо посилання на функцію відправки екшенів
   const dispatch = useDispatch();
-  // Викликаємо генератор екшену та передаємо ідентифікатор завдання
-  // Відправляємо результат - екшен видалення завдання
+
   const handleDelete = () => dispatch(deleteTask(task.id));
-  // Викликаємо генератор екшену та передаємо ідентифікатор завдання
-  // Відправляємо результат - екшен перемикання статусу завдання
+
   const handleToggle = () => dispatch(toggleCompleted(task.id));
 
   return (
